8945005a8c84929ffb363de6753ce67d036951e6
Mohit Sangwan <99173518+Mohit-Sangwan-1@users.noreply.github.com>
2025-03-13 07:58:10 +0000

    final submission of data type summary with some new information

--- basics_01/dataTypesSummary.js
+++ basics_01/dataTypesSummary.js
@@ -27,8 +27,45 @@ reference are allocated directly in memory
 2.Object
 3.Function
 
+all non primitive data types have typeof function 
+but we get result as object in console while checking through console
 ###### if you want to master javascript then master object and arrays 
 
+
+/*
+
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
+
+/*
+
+JavaScript is a dynamically typed language. This means that variable types are determined at runtime, and you do not need to explicitly declare the type of a variable before using it. You can assign different types of values to a variable during its lifetime.
+
+For example, in JavaScript, you can do the following:
+
+let x = 10; // x is now a number
+x = "Hello"; // x is now a string
+x = true; // x is now a boolean
+On the other hand, statically typed languages require you to declare the variable's type explicitly, and the type checking is done at compile-time, before the code is executed.
+
+Languages like Java, C++, and TypeScript are statically typed, and they require you to specify the variable type explicitly when declaring them:
+
+int x = 10; // x is a variable of type int
+String name = "John"; // name is a variable of type String
+JavaScript's dynamic typing allows for more flexibility but can lead to potential runtime errors if not handled carefully. Static typing, on the other hand, provides better type safety at the cost of some initial verbosity and strictness.
 */
 
 // Type of javascript :Dynamically typed language or static type language
@@ -62,8 +99,8 @@ const myFunction=function(){
   
 }
 
-console.log("Mohit Sangwan");
-console.log("Mohit Sangwan");
+console.log(typeof myFunction); // it returns datatype as function but it is called objectFunction
+// console.log("Mohit Sangwan");
 
 
 
